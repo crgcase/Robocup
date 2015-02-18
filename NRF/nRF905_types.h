3d0cb4952bde7e2d5a859dbc635a553428e13d48
Hassan Niazi <Sikander_niazi1@hotmail.com>
2015-02-19 03:19:42 +0500

    Create nRF905_types.h

---
+++ NRF/nRF905_types.h
@@ -0,0 +1,15 @@
+#ifndef NRF905_TYPES_H_
+#define NRF905_TYPES_H_
+
+#ifndef ARDUINO
+#include <stdbool.h>
+#endif
+#include "nRF905_config.h"
+/*
+typedef struct {
+	uint8_t len;
+	uint8_t dstAddress[NRF905_ADDR_SIZE];
+	uint8_t buffer[64];
+} nrf905_packet_s;
+*/
+#endif /* NRF905_TYPES_H_ */
